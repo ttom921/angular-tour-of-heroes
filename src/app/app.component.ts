7ab1ca8d4ed9ebf73225103e03f281d847145a95
ttom921 <ttom921@hotmail.com>
2018-03-12 09:19:01 +0800

    加入第二步

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,9 @@ export class Hero {
   name: string;
 }
 
-<<<<<<< HEAD
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-=======
 
->>>>>>> cbccfe241967e093a4628c766483901d5225cd23
+
+
 
 @Component({
   selector: 'app-root',
@@ -29,14 +16,7 @@ const HEROES: Hero[] = [
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-<<<<<<< HEAD
-  heroes = HEROES;
-=======
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
->>>>>>> cbccfe241967e093a4628c766483901d5225cd23
+
 }
 
 
