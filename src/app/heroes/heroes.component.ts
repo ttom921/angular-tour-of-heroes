5d09e310929695420ab73288fb6e57d196dc4571
ttom921 <ttom921@hotmail.com>
2018-03-28 09:35:18 +0800

    04顯示英雄列表

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROS } from './../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,14 +8,15 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Widnstorm'
-  };
+  heroes = HEROS;
+  selectedHero: Hero;
+
   constructor() { }
 
   ngOnInit() {
 
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
